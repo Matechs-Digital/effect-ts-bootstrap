889cd21947c2a236fce9476676a29b85df426304
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-09 16:55:10 +0100

    improve demo

--- src/program.ts
+++ src/program.ts
@@ -1,36 +1,27 @@
 import * as T from "@effect-ts/core/Effect"
 import { pipe } from "@effect-ts/core/Function"
 
-import {
-  add,
-  base,
-  Calculator,
-  factor,
-  factorFun,
-  gen,
-  LiveCalculator,
-  mul
-} from "./calculator"
-import { Console, LiveConsole, log } from "./console"
+import * as Calc from "./calculator"
+import * as Console from "./console"
 
 // program
 export const program = pipe(
   T.zip_(
-    gen((_) => _(1)),
-    factorFun((_) => _())
+    Calc.gen((_) => _(1)),
+    Calc.factorFun((_) => _())
   ),
-  T.chain((_) => T.tuple(base, factor, T.succeed(_))),
-  T.chain(([b, f, [x, y]]) => T.chain_(add(b, f), (k) => T.succeed(k + x + y))),
-  T.chain((sum) => mul(sum, 3)),
-  T.chain((n) => log(`Result: ${n}`))
+  T.chain((_) => T.tuple(Calc.base, Calc.factor, T.succeed(_))),
+  T.chain(([b, f, [x, y]]) => T.chain_(Calc.add(b, f), (k) => T.succeed(k + x + y))),
+  T.chain((sum) => Calc.mul(sum, 3)),
+  T.chain((n) => Console.log(`Result: ${n}`))
 )
 
 // main function (unsafe)
 export function main() {
   return pipe(
     program,
-    T.provideService(Calculator)(LiveCalculator()),
-    T.provideService(Console)(LiveConsole()),
+    T.provideService(Calc.Calculator)(Calc.LiveCalculator()),
+    T.provideService(Console.Console)(Console.LiveConsole()),
     T.runMain
   )
 }
