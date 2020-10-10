b6f40a919a8a0b3857cc751129f9fe44f7af2e96
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-10 18:57:51 +0100

    segment tests

--- test/db.test.ts
+++ test/db.test.ts
@@ -24,156 +24,162 @@ const runtime = pipe(
   testRuntime
 )
 
-describe("Live Db", () => {
-  it("migrations are being applied", async () => {
-    expect(
-      await pipe(
-        T.accessService(Migrations)((_) => _.migrations.length),
-        runtime.runPromise
+describe("Integration Suite", () => {
+  describe("Bootstrap", () => {
+    it("migrations are being applied", async () => {
+      expect(
+        await pipe(
+          T.accessService(Migrations)((_) => _.migrations.length),
+          runtime.runPromise
+        )
+      ).toEqual(1)
+    })
+
+    it("run simple query", async () => {
+      const response = await pipe(
+        PgClient.accessM((client) =>
+          pipe(
+            T.fromPromiseDie(() =>
+              client.query("SELECT $1::text as name", ["Michael"])
+            ),
+            T.map((_): string => _.rows[0].name)
+          )
+        ),
+        PgClient.provide,
+        runtime.runPromiseExit
       )
-    ).toEqual(1)
+
+      expect(response).toEqual(Ex.succeed("Michael"))
+    })
+
+    it("check users table structure", async () => {
+      const response = await pipe(
+        PgClient.accessM((client) =>
+          pipe(
+            T.fromPromiseDie(() =>
+              client.query(
+                "SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name = $1::text;",
+                ["users"]
+              )
+            ),
+            T.map((_) => _.rows)
+          )
+        ),
+        PgClient.provide,
+        runtime.runPromiseExit
+      )
+
+      expect(response).toEqual(
+        Ex.succeed([
+          { table_name: "users", column_name: "id", data_type: "integer" },
+          {
+            table_name: "users",
+            column_name: "name",
+            data_type: "character varying"
+          },
+          {
+            table_name: "users",
+            column_name: "createdAt",
+            data_type: "timestamp without time zone"
+          }
+        ])
+      )
+    })
+
+    it("check posts table structure", async () => {
+      const response = await pipe(
+        PgClient.accessM((client) =>
+          pipe(
+            T.fromPromiseDie(() =>
+              client.query(
+                "SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name = $1::text;",
+                ["posts"]
+              )
+            ),
+            T.map((_) => _.rows)
+          )
+        ),
+        PgClient.provide,
+        runtime.runPromiseExit
+      )
+
+      expect(response).toEqual(
+        Ex.succeed([
+          { table_name: "posts", column_name: "id", data_type: "integer" },
+          {
+            table_name: "posts",
+            column_name: "userId",
+            data_type: "integer"
+          },
+          {
+            table_name: "posts",
+            column_name: "body",
+            data_type: "text"
+          },
+          {
+            column_name: "createdAt",
+            data_type: "timestamp without time zone",
+            table_name: "posts"
+          }
+        ])
+      )
+    })
   })
 
-  it("run simple query", async () => {
-    const response = await pipe(
-      PgClient.accessM((client) =>
-        pipe(
-          T.fromPromiseDie(() => client.query("SELECT $1::text as name", ["Michael"])),
-          T.map((_): string => _.rows[0].name)
-        )
-      ),
-      PgClient.provide,
-      runtime.runPromiseExit
-    )
+  describe("User Api", () => {
+    it("creates a new user", async () => {
+      const result = await pipe(
+        createUser({ name: "Michael" }),
+        PgClient.provide,
+        runtime.runPromiseExit
+      )
 
-    expect(response).toEqual(Ex.succeed("Michael"))
-  })
+      const nameAndId = pipe(User.lens, Lens.props("name", "id"))
 
-  it("check users table structure", async () => {
-    const response = await pipe(
-      PgClient.accessM((client) =>
-        pipe(
-          T.fromPromiseDie(() =>
-            client.query(
-              "SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name = $1::text;",
-              ["users"]
-            )
-          ),
-          T.map((_) => _.rows)
-        )
-      ),
-      PgClient.provide,
-      runtime.runPromiseExit
-    )
+      expect(pipe(result, Ex.map(nameAndId.get))).toEqual(
+        Ex.succeed({ id: 1, name: "Michael" })
+      )
+    })
 
-    expect(response).toEqual(
-      Ex.succeed([
-        { table_name: "users", column_name: "id", data_type: "integer" },
-        {
-          table_name: "users",
-          column_name: "name",
-          data_type: "character varying"
-        },
-        {
-          table_name: "users",
-          column_name: "createdAt",
-          data_type: "timestamp without time zone"
-        }
-      ])
-    )
-  })
+    it("fail to create a new user with an ampty name", async () => {
+      const result = await pipe(
+        createUser({ name: "" }),
+        PgClient.provide,
+        runtime.runPromiseExit
+      )
 
-  it("check posts table structure", async () => {
-    const response = await pipe(
-      PgClient.accessM((client) =>
-        pipe(
-          T.fromPromiseDie(() =>
-            client.query(
-              "SELECT table_name, column_name, data_type FROM information_schema.columns WHERE table_name = $1::text;",
-              ["posts"]
-            )
-          ),
-          T.map((_) => _.rows)
-        )
-      ),
-      PgClient.provide,
-      runtime.runPromiseExit
-    )
+      expect(result).toEqual(
+        Ex.fail(new ValidationError("name should be between 0 and 255 characters long"))
+      )
+    })
 
-    expect(response).toEqual(
-      Ex.succeed([
-        { table_name: "posts", column_name: "id", data_type: "integer" },
-        {
-          table_name: "posts",
-          column_name: "userId",
-          data_type: "integer"
-        },
-        {
-          table_name: "posts",
-          column_name: "body",
-          data_type: "text"
-        },
-        {
-          column_name: "createdAt",
-          data_type: "timestamp without time zone",
-          table_name: "posts"
-        }
-      ])
-    )
-  })
+    it("create arbitrary users", async () => {
+      await fc.check(
+        fc.asyncProperty(arbitrary(CreateUser), async (_) => {
+          const result = await pipe(
+            createUser(_),
+            PgClient.provide,
+            runtime.runPromiseExit
+          )
 
-  it("creates a new user", async () => {
-    const result = await pipe(
-      createUser({ name: "Michael" }),
-      PgClient.provide,
-      runtime.runPromiseExit
-    )
+          expect(result._tag).toEqual("Success")
+        })
+      )
+    })
 
-    const nameAndId = pipe(User.lens, Lens.props("name", "id"))
+    it("find users created from previous steps", async () => {
+      const response = await pipe(
+        PgClient.accessM((client) =>
+          pipe(
+            T.fromPromiseDie(() => client.query("SELECT COUNT(*) FROM users")),
+            T.map((_) => parseInt(_.rows[0].count))
+          )
+        ),
+        PgClient.provide,
+        runtime.runPromiseExit
+      )
 
-    expect(pipe(result, Ex.map(nameAndId.get))).toEqual(
-      Ex.succeed({ id: 1, name: "Michael" })
-    )
-  })
-
-  it("fail to create a new user with an ampty name", async () => {
-    const result = await pipe(
-      createUser({ name: "" }),
-      PgClient.provide,
-      runtime.runPromiseExit
-    )
-
-    expect(result).toEqual(
-      Ex.fail(new ValidationError("name should be between 0 and 255 characters long"))
-    )
-  })
-
-  it("create arbitrary users", async () => {
-    await fc.check(
-      fc.asyncProperty(arbitrary(CreateUser), async (_) => {
-        const result = await pipe(
-          createUser(_),
-          PgClient.provide,
-          runtime.runPromiseExit
-        )
-
-        expect(result._tag).toEqual("Success")
-      })
-    )
-  })
-
-  it("find users created from previous steps", async () => {
-    const response = await pipe(
-      PgClient.accessM((client) =>
-        pipe(
-          T.fromPromiseDie(() => client.query("SELECT COUNT(*) FROM users")),
-          T.map((_) => parseInt(_.rows[0].count))
-        )
-      ),
-      PgClient.provide,
-      runtime.runPromiseExit
-    )
-
-    expect(response).toEqual(Ex.succeed(101))
+      expect(response).toEqual(Ex.succeed(101))
+    })
   })
 })
