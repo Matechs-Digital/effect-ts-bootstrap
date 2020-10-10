fe3ef16c254cbfd6eaff1bc521c0a51243638ea6
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-10 17:57:49 +0100

    minor restyle

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,26 +1,27 @@
 import { has } from "@effect-ts/core/Classic/Has"
 import * as T from "@effect-ts/core/Effect"
 import * as L from "@effect-ts/core/Effect/Layer"
-import { pipe } from "@effect-ts/core/Function"
+import { flow, pipe } from "@effect-ts/core/Function"
 
 import * as PG from "../db/PgClient"
 import type { CreateUser } from "../model/user"
 import { decodeUser } from "../model/user"
 
-export function makeUserPersistence() {
-  return {
-    createUser(_: CreateUser) {
-      return pipe(
-        PG.accessM((db) =>
-          T.fromPromiseDie(() =>
-            db.query(`INSERT INTO users (name) VALUES ($1::text) RETURNING *`, [_.name])
+export const makeUserPersistence = () => ({
+  createUser: (_: CreateUser) =>
+    pipe(
+      PG.accessM((db) =>
+        T.fromPromiseDie(async () => {
+          const result = await db.query(
+            `INSERT INTO users (name) VALUES ($1::text) RETURNING *`,
+            [_.name]
           )
-        ),
-        T.chain((_) => T.orDie(decodeUser(_.rows[0])))
-      )
-    }
-  }
-}
+          return result.rows[0]
+        })
+      ),
+      T.chain(flow(decodeUser, T.orDie))
+    )
+})
 
 export interface UserPersistence extends ReturnType<typeof makeUserPersistence> {}
 
