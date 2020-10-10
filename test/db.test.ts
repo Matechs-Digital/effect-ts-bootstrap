4487a16944f217cd59ce51d04037ed5171b108ad
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-10 14:50:26 +0100

    add migrations to test

--- test/db.test.ts
+++ test/db.test.ts
@@ -6,7 +6,7 @@ import { pipe } from "@effect-ts/core/Function"
 import * as PG from "../src/db/client"
 import { TestContainersLive } from "./utils/containers"
 import { PgConfigTest } from "./utils/db"
-import { TestMigration } from "./utils/migration"
+import { Migrations, TestMigration } from "./utils/migration"
 import { testRuntime } from "./utils/runtime"
 
 const runtime = pipe(
@@ -18,6 +18,14 @@ const runtime = pipe(
 )
 
 describe("Live Db", () => {
+  it("migrations are being applied", async () => {
+    expect(
+      await pipe(
+        T.accessService(Migrations)((_) => _.migrations.length),
+        runtime.runPromise
+      )
+    ).toEqual(1)
+  })
   it("run simple query", async () => {
     const response = await pipe(
       PG.withClientM((client) =>
