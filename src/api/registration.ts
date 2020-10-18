19c47ad629aad78d817876982b372b5ac0dae390
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-18 21:17:04 +0100

    improve

--- src/api/registration.ts
+++ src/api/registration.ts
@@ -1,7 +1,7 @@
 import * as T from "@effect-ts/core/Effect"
 import { pipe } from "@effect-ts/core/Function"
 
-import { fromPool } from "../db"
+import * as Db from "../db"
 import { addRoute, matchRegex, morphicBody, morphicResponse } from "../http"
 import { Register } from "../model/api"
 import { User } from "../model/user"
@@ -10,7 +10,7 @@ import { register } from "../persistence/transactions"
 export const addRegistration = addRoute(matchRegex(/^\/register$/, ["POST"]))(() =>
   T.gen(function* (_) {
     const body = yield* _(morphicBody(Register))
-    const user = yield* _(pipe(register(body), T.orDie, fromPool("main")))
+    const user = yield* _(pipe(register(body), T.orDie, Db.fromPool("main")))
 
     return yield* _(morphicResponse(User)(user))
   })
