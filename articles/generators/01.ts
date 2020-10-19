f2c3bbbc6d07af6a1afa671fcafcdfada5949b1a
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-19 19:58:50 +0100

    migrate to operator and gen

---
+++ articles/generators/01.ts
@@ -0,0 +1,123 @@
+import "@effect-ts/core/Operators"
+
+import * as Array from "@effect-ts/core/Classic/Array"
+import * as Map from "@effect-ts/core/Classic/Map"
+import * as T from "@effect-ts/core/Effect"
+import * as L from "@effect-ts/core/Effect/Layer"
+import * as M from "@effect-ts/core/Effect/Managed"
+import * as Ref from "@effect-ts/core/Effect/Ref"
+import { pipe } from "@effect-ts/core/Function"
+import type { NoSuchElementException } from "@effect-ts/system/GlobalExceptions"
+import { has } from "@effect-ts/system/Has"
+
+// simulate a database connection to a key-value store
+export interface DbConnection {
+  readonly put: (k: string, v: string) => T.UIO<void>
+  readonly get: (k: string) => T.IO<NoSuchElementException, string>
+  readonly clear: T.UIO<void>
+}
+
+export const DbConnection = has<DbConnection>()
+
+// simulate a connection to a message broker
+export interface BrokerConnection {
+  readonly send: (message: string) => T.UIO<void>
+  readonly clear: T.UIO<void>
+}
+
+export const BrokerConnection = has<BrokerConnection>()
+
+// connect to the database
+export const DbLive = pipe(
+  T.gen(function* (_) {
+    const ref = yield* _(Ref.makeRef<Map.Map<string, string>>(Map.empty))
+
+    const DbConnection: DbConnection = {
+      get: (k) => ref.get["|>"](T.map(Map.lookup(k)))["|>"](T.chain(T.getOrFail)),
+      put: (k, v) => ref["|>"](Ref.update(Map.insert(k, v))),
+      clear: ref.set(Map.empty)
+    }
+
+    return DbConnection
+  }),
+  // release the connection via managed
+  M.make((_) => _.clear),
+  // construct the layer
+  L.fromManaged(DbConnection)
+)
+
+// connect to the database
+export const BrokerLive = pipe(
+  T.gen(function* (_) {
+    const ref = yield* _(Ref.makeRef<Array.Array<string>>(Array.empty))
+
+    const BrokerConnection: BrokerConnection = {
+      send: (message) => ref["|>"](Ref.update(Array.snoc(message))),
+      clear: ref.get["|>"](
+        T.chain((messages) =>
+          T.effectTotal(() => {
+            console.log(`Flush:`)
+            messages.forEach((message) => {
+              console.log("- " + message)
+            })
+          })
+        )
+      )
+    }
+
+    return BrokerConnection
+  }),
+  // release the connection via managed
+  M.make((_) => _.clear),
+  // construct the layer
+  L.fromManaged(BrokerConnection)
+)
+
+export function makeProgram({ get, put }: DbConnection, { send }: BrokerConnection) {
+  return {
+    main: T.gen(function* (_) {
+      yield* _(put("ka", "a"))
+      yield* _(put("kb", "b"))
+      yield* _(put("kc", "c"))
+
+      const a = yield* _(get("ka"))
+      const b = yield* _(get("kb"))
+      const c = yield* _(get("kc"))
+
+      const s = `${a}-${b}-${c}`
+
+      yield* _(send(s))
+
+      return s
+    })
+  }
+}
+
+export interface Program extends ReturnType<typeof makeProgram> {}
+
+export const Program = has<Program>()
+
+export const ProgramLive = L.fromConstructor(Program)(makeProgram)(
+  DbConnection,
+  BrokerConnection
+)
+
+export const MainLive = pipe(ProgramLive, L.using(L.all(DbLive, BrokerLive)))
+
+export const { main } = T.deriveLifted(Program)([], ["main"], [])
+
+// run the program and print the output
+pipe(
+  T.gen(function* (_) {
+    const s = yield* _(main)
+
+    yield* _(
+      T.effectTotal(() => {
+        console.log(`Done: ${s}`)
+      })
+    )
+  }),
+  // provide the layer to program
+  T.provideSomeLayer(MainLive),
+  T.runMain
+)
