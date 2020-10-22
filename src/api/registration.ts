06a384f9e1148b2f2cab91214a61514eafe58778
Michael Arnaldi <arnaldimichael@gmail.com>
2020-10-22 15:00:42 +0100

    improve

--- src/api/registration.ts
+++ src/api/registration.ts
@@ -2,7 +2,7 @@ import "@effect-ts/core/Operators"
 
 import * as T from "@effect-ts/core/Effect"
 
-import { withPoolClient } from "../db"
+import { Db } from "../db"
 import { addRoute, jsonBody, jsonResponse, matchRegex } from "../http"
 import { Register } from "../model/api"
 import { User } from "../model/user"
@@ -10,8 +10,10 @@ import { register } from "../persistence/transactions"
 
 export const addRegistration = addRoute(matchRegex(/^\/register$/, ["POST"]))(() =>
   T.gen(function* (_) {
+    const { withPoolClient } = yield* _(Db("main"))
+
     const body = yield* _(jsonBody(Register))
-    const user = yield* _(register(body)["|>"](T.orDie)["|>"](withPoolClient("main")))
+    const user = yield* _(register(body)["|>"](T.orDie)["|>"](withPoolClient))
 
     return yield* _(jsonResponse(User)(user))
   })
--- src/db/api.ts
+++ src/db/api.ts
@@ -1,6 +1,7 @@
 import type { Has } from "@effect-ts/core/Classic/Has"
 import { has } from "@effect-ts/core/Classic/Has"
 import * as T from "@effect-ts/core/Effect"
+import type { Clock } from "@effect-ts/core/Effect/Clock"
 import * as L from "@effect-ts/core/Effect/Layer"
 import type { QueryResult, QueryResultRow } from "pg"
 
@@ -8,29 +9,41 @@ import { deriveTenants } from "../tenants"
 import * as Pg from "./client"
 import type { Databases } from "./database"
 import { databases } from "./database"
+import { PgPool } from "./pool"
 
 export const apis = deriveTenants(databases)
 
 export type Supported = number | string | boolean | null | undefined
 
-export function makeLiveDb<K extends Databases>(db: K): () => Db<K> {
-  const query = (queryString: string, ...args: Supported[]) =>
-    Pg.accessClientM(db)((client) =>
-      T.fromPromiseDie(
-        (): Promise<QueryResult<QueryResultRow>> => client.query(queryString, args)
+export function makeLiveDb<K extends Databases>(db: K) {
+  return T.gen(function* (_) {
+    const { withPoolClientM } = yield* _(PgPool(db))
+
+    const query = (queryString: string, ...args: Supported[]) =>
+      Pg.accessClientM(db)((client) =>
+        T.fromPromiseDie(
+          (): Promise<QueryResult<QueryResultRow>> => client.query(queryString, args)
+        )
       )
-    )
 
-  const transaction = <R, E, A>(body: T.Effect<R, E, A>) =>
-    T.bracketExit_(
-      query("BEGIN"),
-      () => body,
-      (_, e) => (e._tag === "Success" ? query("COMMIT") : query("ROLLBACK"))
-    )
+    const transaction = <R, E, A>(body: T.Effect<R, E, A>) =>
+      T.bracketExit_(
+        query("BEGIN"),
+        () => body,
+        (_, e) => (e._tag === "Success" ? query("COMMIT") : query("ROLLBACK"))
+      )
 
-  return () => ({
-    query,
-    transaction
+    function withPoolClient<R, E, A>(self: T.Effect<R & Has<Pg.PgClient<K>>, E, A>) {
+      return withPoolClientM((_) =>
+        T.provideService(Pg.PgClient(db))({ _tag: db, client: _ })(self)
+      )
+    }
+
+    return {
+      query,
+      transaction,
+      withPoolClient
+    }
   })
 }
 
@@ -43,9 +56,13 @@ export interface Db<K extends Databases> {
   transaction: <R, E, A>(
     body: T.Effect<R, E, A>
   ) => T.Effect<R & Has<Pg.PgClient<K>>, E, A>
+
+  withPoolClient: <R, E, A>(
+    self: T.Effect<R & Has<Pg.PgClient<K>>, E, A>
+  ) => T.Effect<R & Has<Clock> & Has<PgPool<K>>, E, A>
 }
 
 export const Db = <K extends Databases>(db: K) => has<Db<K>>().setKey(apis[db])
 
 export const DbLive = <K extends Databases>(db: K) =>
-  L.fromConstructor(Db(db))(makeLiveDb(db))()
+  L.fromEffect(Db(db))(makeLiveDb(db))
